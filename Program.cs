c93f9600657efc1264427e1099c833967cb5c775
taylor Schuchter <taylorschuchter@gmail.com>
2023-03-20 18:06:05 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+ public static string FizzOrBuzz(int x)
+    {
+        if (x % 5 == 0 && x % 3 == 0)
+        {
+            return "fizzbuzz";
+        }
+        else if (x % 5 == 0)
+        {
+            return "buzz";
+        }
+        else if (x % 3 == 0)
+        {
+            return "fizz";
+        }
+        else 
+        return "";      
+        
+    }
+static void Main(string[] args)
+{
+   Console.WriteLine(FizzOrBuzz(33));
+   Console.WriteLine(FizzOrBuzz(45));
+   Console.WriteLine(FizzOrBuzz(25));
+   Console.WriteLine(FizzOrBuzz(2));
+}
+}
+}
